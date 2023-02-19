6c438124f588cd590a1cff396d6ddc85f422374d
Hakan Çetin <netcat16@gmail.com>
2023-02-19 21:54:36 +0300

    updated

--- lib/a1/utils/constants.dart
+++ lib/a1/utils/constants.dart
@@ -20,8 +20,7 @@ const privacyPolicyURL = "https://inmansdj.herokuapp.com/privacy-policy.pdf";
 // check if Said directory is exist. If exist then you are working on local
 String file = '/home/cappittall/Documents/Said/.local';
 
-const bool isLocal = false;
-
+const bool isLocal = true;
 // Database connection constans
 const url_server = "https://inmansdj.herokuapp.com/i";
 const url_local = "http://192.168.1.154:8000/i";
