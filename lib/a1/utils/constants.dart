845c3b086a1beb7405eabb4cb18cac2e7be225fd
Hakan Çetin <netcat16@gmail.com>
2023-02-22 10:02:15 +0300

    updated

--- lib/a1/utils/constants.dart
+++ lib/a1/utils/constants.dart
@@ -20,7 +20,7 @@ const privacyPolicyURL = "https://euronatuur.com/i/privacy-policy.pdf";
 // check if Said directory is exist. If exist then you are working on local
 String file = '/home/cappittall/Documents/Said/.local';
 
-const bool isLocal = false;
+const bool isLocal = true;
 // Database connection constans
 const url_server = "https://euronatuur.com/i";
 const url_local = "http://192.168.1.154:8000/i";
